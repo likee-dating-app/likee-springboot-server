befb2f64e3ecd11d4c1b076074da0b969795dc6c
Rupesh Kumar Deshmukh <contact@rupeshdeshmukh.dev>
2024-09-21 15:28:01 +0530

    initial commit

---
+++ src/main/kotlin/com/rdktechnologies/BackendTemplate/BackendTemplateApplication.kt
@@ -0,0 +1,26 @@
+package com.rdktechnologies.BackendTemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class BackendTemplateApplication{
+
+
+
+
+	@GetMapping("/hello")
+	fun  hello(@RequestParam(value = "name", defaultValue = "World") name:String):String {
+		return String.format("Hello %s!", name);
+	}
+}
+
+fun main(args: Array<String>) {
+	runApplication<BackendTemplateApplication>(*args)
+}
+
+
